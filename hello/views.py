00a34cfe567ec869f3b53559fe80120f4aa4fddf
swarna <starwars121012@gmail.com>
2020-07-17 16:43:59 +0530

    first commit

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hi(request):
-    return HttpResponse("Hi...")
+    return HttpResponse("Hi Django !!")
 
 def say_hi_student(request,student):
    # return HttpResponse('Hi ' + student)
